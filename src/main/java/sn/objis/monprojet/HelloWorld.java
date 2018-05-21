40cbf831cf9981face2389d2852662102a88de7f
Laye WANE <laye1wane@gmail.com>
2018-05-21 18:18:03 +0000

    Ajout du message Hello world

--- src/main/java/sn/objis/monprojet/HelloWorld.java
+++ src/main/java/sn/objis/monprojet/HelloWorld.java
@@ -1,10 +1,14 @@
 package sn.objis.monprojet;
 
+/**
+ * @author OBJIS isWANE
+ *
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
