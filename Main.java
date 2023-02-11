d48de630c7b08b6cf0deca2a46ea7e09c3a6f14c
Abdoulaye WANE <37803499+iswane@users.noreply.github.com>
2023-02-11 16:19:32 +0000

    Update Main.java
    Update file for running trigger

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Team jenkins, voici notre first trigger Git!");
+		System.out.println("Changement pour déclencher le trigger git");
 	}
 
 }
